ec63c592b6980076248236d5dc2e2aa13a64899f
syed-jaid <syed.jaid.7748@gmail.com>
2024-03-22 18:00:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#edffdd] p-3 text-center">
+      <img
+        className="w-[50px]"
+        src="https://i.ibb.co/kxtWZwg/Screenshot-2024-03-22-at-5-14-50-PM.png"
+      ></img>
+
+      <h1 className="mt-4 text-3xl sm:text-4xl font-bold text-black">
+        Teach me to count
+      </h1>
+      <p className="mt-4 w-[310px] text-[20px] sm:text-3xl text-black">
+        Get 3 chances to guess the right answers. Learn counting now!
+      </p>
+      <div className="mt-10 w-full items-center max-w-[450px] gap-3 flex flex-col-reverse sm:flex-row">
+        <button className="h-10 border-[1px] border-black py-2 px-8 text-sm text-black rounded-[20px] whitespace-nowrap w-[150px]">
+          How to play
+        </button>
+        <button className="h-10 border-[1px] border-black py-2 px-8 text-sm text-black rounded-[20px] whitespace-nowrap w-[150px]">
+          Log in
+        </button>
+        <button className="h-10 border-[1px] border-black py-2 px-8 text-sm text-white rounded-[20px] whitespace-nowrap w-[150px] bg-black">
+          Play
+        </button>
+      </div>
+      <p className="mt-10 text-sm text-black">
+        {new Date().toLocaleDateString()}
+      </p>
     </div>
   );
 }
