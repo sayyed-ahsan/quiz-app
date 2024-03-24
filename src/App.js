cbccfb1ed069fdcae78e5c889c1830f9bee7b206
syed-jaid <syed.jaid.7748@gmail.com>
2024-03-24 10:23:12 +0600

    home page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-/* eslint-disable jsx-a11y/alt-text */
-import "./App.css";
+import React from 'react';
+import HomePage from './components/app/home/home';
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#edffdd] p-2 text-center">
-      <img
-        className="w-[50px]"
-        src="https://i.ibb.co/kxtWZwg/Screenshot-2024-03-22-at-5-14-50-PM.png"
-      ></img>
-
-      <h1 className="mt-4 text-3xl sm:text-4xl font-bold text-black">
-        Teach me to count
-      </h1>
-      <p className="mt-4 w-[310px] text-[20px] sm:text-3xl text-black">
-        Get 3 chances to guess the right answers. Learn counting now!
-      </p>
-      <div className="mt-10 w-full items-center max-w-[450px] gap-3 flex flex-col-reverse sm:flex-row">
-        <button className="h-10 border-[1px] border-black py-2 px-8 text-sm text-black rounded-[20px] whitespace-nowrap w-[150px]">
-          How to play
-        </button>
-        <button className="h-10 border-[1px] border-black py-2 px-8 text-sm text-black rounded-[20px] whitespace-nowrap w-[150px]">
-          Log in
-        </button>
-        <button className="h-10 border-[1px] border-black py-2 px-8 text-sm text-white rounded-[20px] whitespace-nowrap w-[150px] bg-black">
-          Play
-        </button>
+    <>
+      <div className="video-bg">
+        <video width="320" height="240" autoPlay loop muted>
+          <source src="https://assets.codepen.io/3364143/7btrrd.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
-      <p className="mt-10 text-sm text-black">
-        {new Date().toLocaleDateString()}
-      </p>
-    </div>
+      <HomePage />
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+{/* <div class="bg-cover bg-center bg-[url('https://cdn-media-2.freecodecamp.org/w1280/5f9c9cfc740569d1a4ca3543.jpg')]">
+<div className="flex min-h-screen flex-col items-center justify-center p-2 text-center">
+  <img
+    className="w-[50px]"
+    src="https://i.ibb.co/kxtWZwg/Screenshot-2024-03-22-at-5-14-50-PM.png"
+  ></img>
+
+  <h1 className="mt-4 text-3xl sm:text-4xl font-bold text-white">
+    Teach me to count
+  </h1>
+  <p className="mt-4 w-[310px] text-[20px] sm:text-xl text-white">
+    Get 3 chances to guess the right answers. Learn counting now!
+  </p>
+  <div className="mt-10 w-full items-center max-w-[450px] gap-3 flex flex-col-reverse sm:flex-row">
+    <button className="h-10 border-[1px] border-white py-2 px-8 text-sm text-white rounded-[20px] whitespace-nowrap w-[150px]">
+      How to play
+    </button>
+    <button className="h-10 border-[1px] border-white py-2 px-8 text-sm text-white rounded-[20px] whitespace-nowrap w-[150px]">
+      Log in
+    </button>
+    <button className="h-10 border-[1px] border-white py-2 px-8 text-sm text-white rounded-[20px] whitespace-nowrap w-[150px] bg-black">
+      Play
+    </button>
+  </div>
+  <p className="mt-10 text-sm text-white">
+    {new Date().toLocaleDateString()}
+  </p>
+</div>
+</div> */}
---
+++ src/components/app/home/howToPlayModal.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { IoClose } from 'react-icons/io5';
+import { AiFillLike, AiFillDislike } from "react-icons/ai";
+
+const HowToPlayModal = ({ isOpen, onClose, children }) => {
+    const [like, setLike] = useState('')
+    if (!isOpen) return null;
+
+    return (
+        <div className="fixed inset-0  flex justify-center items-center">
+            <div className="howToPlayModalDiv p-5 rounded-lg shadow-lg">
+
+                {/* modal close button  */}
+                <IoClose onClick={onClose} className='hover:bg-gray-400 rounded-full text-white text-[28px] absolute top-[7px] right-[7px]' />
+
+                {/* body part */}
+                <p className='text-white text-[25px]'>How To Play</p>
+                <p className='text-gray-200 text-[16px] max-h-[500px] overflow-y-auto'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam pariatur, cumque alias reiciendis porro cupiditate vero atque quis eligendi accusamus doloremque, eius maiores nam eaque facere voluptatem, quam neque doloribus saepe possimus dicta! Possimus nisi corrupti cupiditate officiis vero similique nesciunt libero, voluptate, praesentium in quia quaerat qui, pariatur neque quisquam. Totam atque incidunt mollitia, laudantium vel explicabo quibusdam alias enim, rerum ut necessitatibus temporibus assumenda? Magnam eveniet unde quisquam, quo dolore porro earum quasi molestiae saepe veniam, repellendus repudiandae nobis deleniti nemo. Adipisci libero iste similique laudantium debitis illum itaque et quia deleniti expedita sequi vero mollitia, consectetur consequuntur voluptatem ipsa quidem molestiae quo veniam quod inventore corporis. Nesciunt vel aut,
+                </p>
+
+                {/* Feedback section  */}
+                <div className='text-white text-[15px] flex gap-3 fixed bottom-[10px] right-[10px]'>
+                    <p>Did you find it help full ?</p>
+
+                    <AiFillLike onClick={() => setLike('like')} className={`text-[20px] ${like === 'like' && 'text-blue-500'}`} />
+                    <AiFillDislike onClick={() => setLike('disLike')} className={`text-[20px] ${like === 'disLike' && 'text-blue-500'}`} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HowToPlayModal;
